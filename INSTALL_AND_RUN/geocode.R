1a9d1f3f22c8a57db73a92041a68c37b6024efbf
Cole Brokamp <cole.brokamp@gmail.com>
2016-04-20 11:03:05 -0400

    add new arg parser and only geocode unique addresses

--- INSTALL_AND_RUN/geocode.R
+++ INSTALL_AND_RUN/geocode.R
@@ -4,20 +4,22 @@
 # requires Rscript and R
 # requires CB package
 
-args <- commandArgs(TRUE)
+library(argparser)
+p <- arg_parser('offline geocoding, returns the input file with geocodes appended')
+p <- add_argument(p,'file_name',help='name of input csv file')
+p <- add_argument(p,'column_name',help='the name of the column in the csv file that contains the address strings')
+p <- add_argument(p,'--CAGIS',help='try to geocode based on CAGIS files?',default=FALSE,flag=TRUE)
+args <- parse_args(p)
 
-if(length(args) == 0) {
-    cat("geocodes a CSV file of addresses; returns same file but with geocode information appended\n")
-    cat("usage: ./geocode.R \"name_of_input_file.csv\" \"name_of_address_column\"\n")
-    q()
-    }
+in.file <- args$file_name
+address.col.name <- args$column_name
 
-in.file <- args[1]
-address.col.name <- args[2]
 addresses <- read.csv(in.file,stringsAsFactors=FALSE)
 
-geocoded <- CB::CBapply(addresses[ ,address.col.name],function(x) {
-	print(paste0('geocoding ',tail(which(addresses[ ,address.col.name]==x),1),' of ',nrow(addresses)))
+addresses.unique <- unique(addresses[ ,address.col.name])
+
+geocoded <- CB::CBapply(addresses.unique[ ,address.col.name],function(x) {
+	print(paste0('geocoding ',tail(which(addresses.unique[ ,address.col.name]==x),1),' of ',length(addresses.unique)))
 	system(paste0('ruby ~/geocoder/INSTALL_AND_RUN/geocode.rb "',x,'"'))
 	out <- jsonlite::fromJSON('temp.json')
 	return(as.data.frame(out)[1, ])	
@@ -25,13 +27,13 @@ geocoded <- CB::CBapply(addresses[ ,address.col.name],function(x) {
 
 system('rm temp.json')
 
-out.file <- cbind(addresses,geocoded)
+out.file <- merge(addresses,geocoded,by=address.col.name,all=TRUE)
 
 out.file.name <- paste0(gsub('.csv','',in.file,fixed=TRUE),'_geocoded.csv')
 write.csv(out.file,out.file.name)
 
 system(paste0('csv_to_shp ',out.file.name))
 
-print(paste0('FINISHED! output written to ',out.file.name,' and to folder ',paste0(gsub('.csv','',out.file.name,fixed=TRUE)),' as a shapefile'))
+print(paste0('FINISHED! \n    output written to ',out.file.name,'\n and to folder ',paste0(gsub('.csv','',out.file.name,fixed=TRUE)),' as a shapefile'))
 
 
