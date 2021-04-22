34f9c75ff4fb07a4ef4255c9c3614d4332bb1573
Erika <erika.rasnick@gmail.com>
2021-04-22 13:46:05 -0400

    updated sys deps

--- src/liblwgeom/wktparse.tab.h
+++ src/liblwgeom/wktparse.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.5.1.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,46 +31,58 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_LWG_PARSE_YY_Y_TAB_H_INCLUDED
+# define YY_LWG_PARSE_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int lwg_parse_yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     POINT = 258,
-     LINESTRING = 259,
-     POLYGON = 260,
-     MULTIPOINT = 261,
-     MULTILINESTRING = 262,
-     MULTIPOLYGON = 263,
-     GEOMETRYCOLLECTION = 264,
-     CIRCULARSTRING = 265,
-     COMPOUNDCURVE = 266,
-     CURVEPOLYGON = 267,
-     MULTICURVE = 268,
-     MULTISURFACE = 269,
-     POINTM = 270,
-     LINESTRINGM = 271,
-     POLYGONM = 272,
-     MULTIPOINTM = 273,
-     MULTILINESTRINGM = 274,
-     MULTIPOLYGONM = 275,
-     GEOMETRYCOLLECTIONM = 276,
-     CIRCULARSTRINGM = 277,
-     COMPOUNDCURVEM = 278,
-     CURVEPOLYGONM = 279,
-     MULTICURVEM = 280,
-     MULTISURFACEM = 281,
-     SRID = 282,
-     EMPTY = 283,
-     VALUE = 284,
-     LPAREN = 285,
-     RPAREN = 286,
-     COMMA = 287,
-     EQUALS = 288,
-     SEMICOLON = 289,
-     WKB = 290
-   };
+  enum yytokentype
+  {
+    POINT = 258,
+    LINESTRING = 259,
+    POLYGON = 260,
+    MULTIPOINT = 261,
+    MULTILINESTRING = 262,
+    MULTIPOLYGON = 263,
+    GEOMETRYCOLLECTION = 264,
+    CIRCULARSTRING = 265,
+    COMPOUNDCURVE = 266,
+    CURVEPOLYGON = 267,
+    MULTICURVE = 268,
+    MULTISURFACE = 269,
+    POINTM = 270,
+    LINESTRINGM = 271,
+    POLYGONM = 272,
+    MULTIPOINTM = 273,
+    MULTILINESTRINGM = 274,
+    MULTIPOLYGONM = 275,
+    GEOMETRYCOLLECTIONM = 276,
+    CIRCULARSTRINGM = 277,
+    COMPOUNDCURVEM = 278,
+    CURVEPOLYGONM = 279,
+    MULTICURVEM = 280,
+    MULTISURFACEM = 281,
+    SRID = 282,
+    EMPTY = 283,
+    VALUE = 284,
+    LPAREN = 285,
+    RPAREN = 286,
+    COMMA = 287,
+    EQUALS = 288,
+    SEMICOLON = 289,
+    WKB = 290
+  };
 #endif
 /* Tokens.  */
 #define POINT 258
@@ -109,37 +119,40 @@
 #define SEMICOLON 289
 #define WKB 290
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 22 "wktparse.y"
+union YYSTYPE
 {
+#line 22 "wktparse.y"
+
 	double value;
 	const char* wkb;
-}
-/* Line 1489 of yacc.c.  */
-#line 124 "y.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 132 "y.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
 
-extern YYSTYPE lwg_parse_yylval;
-
+/* Location type.  */
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
 {
   int first_line;
   int first_column;
   int last_line;
   int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+};
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
 
+
+extern YYSTYPE lwg_parse_yylval;
 extern YYLTYPE lwg_parse_yylloc;
+int lwg_parse_yyparse (void);
+
+#endif /* !YY_LWG_PARSE_YY_Y_TAB_H_INCLUDED  */
